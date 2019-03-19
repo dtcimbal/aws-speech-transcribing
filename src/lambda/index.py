ae829b767a64cb02643657c080789610ffa780ff
dtsimbal <dtsimbal@gmail.com>
2019-03-19 21:47:44 +1100

    Minor refactoring

--- src/lambda/index.py
+++ src/lambda/index.py
@@ -1,8 +1,8 @@
 import logging
 import os
 import re
+import urllib.parse
 
-import awsutil
 import boto3
 from botocore.config import Config
 
@@ -31,14 +31,59 @@ class ThrottlingException(Exception):
     pass
 
 
-class UnsupportedMediaFormatError(ValueError):
+class UnsupportedMediaFormat(ValueError):
     pass
+
+
+def handler(event, context):
+    # Gets ENV variables
+    output_bucket = os.environ['TRANSCRIPTION_BUCKET']
+
+    # Get the object from the event and show its content type
+    s3obj = event['Records'][0]['s3']
+    source_bucket = s3obj['bucket']['name']
+    source_file = urllib.parse.unquote_plus(s3obj['object']['key'], encoding='utf-8')
+    source_file_format = re.search('\\.(\\w{3,4})$', source_file).group(1)
+
+    # Raises UnsupportedMediaFormat error if the format is unsupported by AWS Transcribe
+    __check_media_format_supported(source_file_format)
+
+    # Formats S3 url
+    source_url = __build_s3url(bucket=source_bucket, key=source_file)
+
+    # Formats the output key
+    output_key = re.sub(r'\s', '.', source_file)
+
+    # Starts transcription job
+    __start_transcription_job(source_url, source_file_format, output_bucket, output_key)
+
+
+def __build_s3url(region: str = region, bucket: str = None, key: str = ''):
+    """
+    Formats URL to download the file from S3 by provided bucket and the key
+    :param region:
+    :param bucket:
+    :param key:
+    :return:
+    """
+    return f"https://s3-{region}.amazonaws.com/{bucket}/{key}"
+
+
+def __check_media_format_supported(media_format):
+    """
+    Ensures provided media_format is supported by Transcribe, throws UnsupportedMediaFormat otherwise
+
+    :param media_format:
+    :return:
+    """
+    if media_format not in SUPPORTED_MEDIA_FORMATS:
+        raise UnsupportedMediaFormat(f"{media_format} is not supported audio type.")
 
 
 def raises_throttling_exception(decorable):
     """
-    Decorates transcribe.exceptions.BadRequestException,transcribe.exceptions.LimitExceededException and
-    transcribe.exceptions.ClientError with the ThrottlingException
+    Decorates transcribe exceptions with the ThrottlingException
+
     :param decorable:
     :return:
     """
@@ -58,7 +103,7 @@ def raises_throttling_exception(decorable):
 
 # Throttling exception wrapper is optional
 @raises_throttling_exception
-def __start_transcription_job(source_url: str, source_format: str, to_bucket: str, to_key: str, lang='en-US'):
+def __start_transcription_job(source_url: str, source_format: str, to_bucket: str, to_key: str, lang='en-US') -> str:
     """
     Starts transcription job
 
@@ -86,24 +131,3 @@ def __start_transcription_job(source_url: str, source_format: str, to_bucket: st
     )
 
     return response['TranscriptionJob']['TranscriptionJobName']
-
-
-def handler(event, context):
-    # Gets ENV variables
-    output_bucket = os.environ['TRANSCRIPTION_BUCKET']
-
-    # Get the object from the event and show its content type
-    (source_bucket, source_key) = awsutil.get_s3path(event)
-    source_format = re.search('\\.(\\w{3,4})$', source_key).group(1)
-
-    if source_format not in SUPPORTED_MEDIA_FORMATS:
-        raise UnsupportedMediaFormatError(f"{source_format} is not supported audio type.")
-
-    log.info("permitted media format: " + source_format)
-    source_url = awsutil.build_s3url(bucket=source_bucket, key=source_key)
-
-    # Formats the output key
-    output_key = re.sub(r'\s', '.', source_key)
-
-    # Starts transcription job
-    __start_transcription_job(source_url, source_format, output_bucket, output_key)
